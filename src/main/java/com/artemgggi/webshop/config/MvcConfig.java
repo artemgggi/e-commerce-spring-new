0aebdbcccab2d24289e10fa339923079f0e6cf98
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-12-16 16:00:04 +0600

    refactored index view

--- src/main/java/com/artemgggi/webshop/config/MvcConfig.java
+++ src/main/java/com/artemgggi/webshop/config/MvcConfig.java
@@ -1,6 +1,9 @@
 package com.artemgggi.webshop.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
@@ -13,26 +16,22 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**",
-//                        "/css/**",
-//                        "/resources/**",
-//                        "/js/**",
-//                        "/images/**",
-//                        "/api/**",
-//                        "/font-awesome/**")
-//
-//                .addResourceLocations(
-//                        "classpath:/static/css/",
-//                        "classpath:/static/js/",
-//                        "classpath:/static/images/",
-//                        "classpath:/static/api/",
-//                        "classpath:/resources/",
-//                        "classpath:/static/font-awesome/");
-//    }
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("src/main/resources");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**",
+                        "/css/**",
+                        "/resources/**",
+                        "/js/**",
+                        "/images/**",
+                        "/api/**",
+                        "/font-awesome/**")
+
+                .addResourceLocations(
+                        "classpath:/static/css/",
+                        "classpath:/static/js/",
+                        "classpath:/static/images/",
+                        "classpath:/static/api/",
+                        "classpath:/resources/",
+                        "classpath:/static/font-awesome/");
     }
 
     @Override
@@ -45,5 +44,11 @@ public class MvcConfig implements WebMvcConfigurer {
         final LocaleChangeInterceptor localeChangeInterceptor =
                 new LocaleChangeInterceptor();
         registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RequestContextListener.class)
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
