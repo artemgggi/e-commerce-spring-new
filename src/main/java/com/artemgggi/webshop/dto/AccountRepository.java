c8195bab275fac0a94246aaab67fcdfa33f85d41
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-12-26 12:51:36 +0600

    added Account repository

---
+++ src/main/java/com/artemgggi/webshop/dto/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.artemgggi.webshop.dto;
+
+import com.artemgggi.webshop.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, String> {
+   }
+
